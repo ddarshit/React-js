5e51fd4d84bb1f89ca210912952c0db47c588c65
ddarshit <darshitdayala007@gmail.com>
2023-08-04 21:09:19 +0530

    my-app

---
+++ src/Functioncomp/Usestate.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./FunctioncompMenu.css";
+import Functioncount from "./Functioncount";
+
+
+const Usestate = () => {
+  return (
+    <>
+      <div className="func-intro">
+        <h1 className="margin2">Usestate</h1>
+        <Functioncount/>
+      </div>
+    </>
+  );
+};
+
+export default Usestate;

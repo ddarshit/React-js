3047ebe4f6287381d13fdd3f63ae7a5d22b66501
ddarshit <darshitdayala007@gmail.com>
2023-08-09 21:48:26 +0530

    my-app

--- src/Functioncomp/FunctioncompMenu.jsx
+++ src/Functioncomp/FunctioncompMenu.jsx
@@ -1,46 +1,54 @@
 import React from "react";
-// import { Component } from "react";
 import { Link, Outlet } from "react-router-dom";
-import "./FunctioncompMenu.css";
 
 const FunctioncompMenu = () => {
-  return ( 
+  return (
     <>
-        <div className="container mt-3 mx-auto"></div>
-        <div className="">
-          <ul className="func-intro">
-            <div className="margin">
-              <li>
-                <Link to="Functioncompintro">
-                  <h3>Functioncompintro</h3>
-                </Link>
-              </li>
-              <li>
-                <Link to="Usestate">
-                  <h3>Usestate</h3>
-                </Link>
-              </li>
-              <li>
-                <Link to="Functioncompprops">
-                  <h3>Functioncompprops</h3>
-                </Link>
-              </li>
-              <li>
-                <Link to="UseEffect">
-                  <h3>UseEffect</h3>
-                </Link>
-              </li>
-              <li>
-                <Link to="LayoutEffect">
-                  <h3>LayoutEffect</h3>
-                </Link>
-              </li>
-            </div>
-          </ul>
-          <Outlet></Outlet>
-        </div>
-      </>
-   );
-}
- 
+      <div className="col-6"></div>
+      <div className="col-6">
+        <ul>
+          <div>
+            <li>
+              <Link to="Functioncompintro">
+                <h3>Functioncompintro</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="Usestate">
+                <h3>Usestate</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="Functioncompprops">
+                <h3>Functioncompprops</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="UseEffect">
+                <h3>UseEffect</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="LayoutEffect">
+                <h3>LayoutEffect</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="Usememo">
+                <h3>Usememo</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="Usecallback">
+                <h3>Usecallback</h3>
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </div>
+      <Outlet></Outlet>
+    </>
+  );
+};
+
 export default FunctioncompMenu;

eac473d0ecee3da5e64a7e2e9ad809e4e88fc8b9
ddarshit <darshitdayala007@gmail.com>
2023-08-13 14:17:02 +0530

    my-app

---
+++ src/Functioncomp/Compone.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Comptwo from './Comptwo';
+
+
+const Compone = () => {
+    return ( 
+        <>
+        <Comptwo/>
+        </>
+     );
+}
+ 
+export default Compone;

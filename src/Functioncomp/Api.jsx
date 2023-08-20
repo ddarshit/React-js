6a9ecbae41ce88629bd8664ed96c6fde264fc7c0
ddarshit <darshitdayala007@gmail.com>
2023-08-20 13:45:28 +0530

    my-app

---
+++ src/Functioncomp/Api.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Apichild from './Apichild';
+
+
+const Api = () => {
+    return ( 
+        <>
+        <h1>API</h1>
+        <Apichild/>
+        </>
+     );
+}
+ 
+export default Api;

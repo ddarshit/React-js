3815b2bf6cc4296ebfe67b52aba3423b97c76067
ddarshit <darshitdayala007@gmail.com>
2023-08-21 12:19:47 +0530

    my-app

---
+++ src/Functioncomp/Functioncompprops.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import User from "./User";
+
+const Functioncompprops = () => {
+  return (
+    <>
+      <div className="col-6"></div>
+      <div className="col-6">
+        <h1>props</h1>
+        <User name="dd" />
+      </div>
+    </>
+  );
+};
+
+export default Functioncompprops;

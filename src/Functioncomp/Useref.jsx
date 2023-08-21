f73a679eaf8f3e6d080cf8e42c65b78deb9b6cf6
ddarshit <darshitdayala007@gmail.com>
2023-08-21 12:20:28 +0530

    my-app

---
+++ src/Functioncomp/Useref.jsx
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+
+const Useref = () => {
+  const inputref = useRef(null);
+
+  function inputFunction() {
+    // inputref.current.value = "100"
+    // inputref.current.focus() 
+    inputref.current.style.color = "blue" 
+    // inputref.current.value.toUppercase()
+  }
+  return (
+    <>
+      <h1>usref</h1>
+      <input type="text" ref={inputref} />
+      <button onClick={inputFunction}>btn</button>
+    </>
+  );
+};
+
+export default Useref;

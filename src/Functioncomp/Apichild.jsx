c521da9debe10dfe38044ac7c4d46b23ba6aa0ef
ddarshit <darshitdayala007@gmail.com>
2023-08-25 14:55:49 +0530

    my-app

--- src/Functioncomp/Apichild.jsx
+++ src/Functioncomp/Apichild.jsx
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from "react";
 const Apichild = () => {
   const [user, setUser] = useState([]);
   // fetch("url")
+  // we take any name instead of data and res
+  // api get then after stored in data. after stored in data convert it into json formate (html view) 
+  // convert in to jason we use it 
+  // view api calle or not : inspect in chrome > network > name 
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos").then((data) => {
@@ -28,7 +32,7 @@ const Apichild = () => {
             <td>{item.userId}</td>
             <td>{item.id}</td>
             <td>{item.title}</td>
-            <td>{item.completed}</td>
+            <td>{item.completed == true ? "True" : "False"}</td>
           </tr>
         ))}
       </table>

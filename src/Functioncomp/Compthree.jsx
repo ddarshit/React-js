69c0f7dd145730e86091507567a776db8c694681
ddarshit <darshitdayala007@gmail.com>
2023-08-28 18:07:24 +0530

    my-app

--- src/Functioncomp/Compthree.jsx
+++ src/Functioncomp/Compthree.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from "react";
-import { Firstname } from "./UsecontextHook";
+import { Firstname, ages } from "./UsecontextHook";
 
 const Compthree = () => {
   const whatever = useContext(Firstname);
+  const what = useContext(ages);
   return (
     <>
       <h1>{whatever}</h1>
+      <h1>{what}</h1>
     </>
   );
 };

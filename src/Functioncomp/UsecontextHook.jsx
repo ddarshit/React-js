b3fc7c88b94b3e18de7f9ad4241ab9e7f6985638
ddarshit <darshitdayala007@gmail.com>
2023-08-28 18:06:37 +0530

    my-app

--- src/Functioncomp/UsecontextHook.jsx
+++ src/Functioncomp/UsecontextHook.jsx
@@ -3,20 +3,24 @@ import Compone from "./Compone";
 
 const Firstname = createContext();
 const Lastname = createContext();
+const ages = createContext();
 
 const UsecontextHook = () => {
-    const name = "Darshit"
-    const Lname = "Dayala"
-    // const obj = {
-    //     age : 22,
-    //     name : "dd"
-    // }
+  const name = "Darshit";
+  const Lname = "Dayala";
+  const age = "22";
+  // const obj = {
+  //     age : 22,
+  //     name : "dd"
+  // }
   return (
     <>
       <Firstname.Provider value={name}>
-      {/* <Firstname.Provider value={obj.age  +  obj.name}> */}
+        {/* <Firstname.Provider value={obj.age  +  obj.name}> */}
         <Lastname.Provider value={Lname}>
-          <Compone />
+          <ages.Provider value={age}>
+            <Compone />
+          </ages.Provider>
         </Lastname.Provider>
       </Firstname.Provider>
     </>
@@ -24,5 +28,6 @@ const UsecontextHook = () => {
 };
 
 export default UsecontextHook;
-export {Firstname}
-export {Lastname}
+export { Firstname };
+export { Lastname };
+export { ages };

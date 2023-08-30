6c5e52b89f6f91304afc9b15554d3d92268090de
ddarshit <darshitdayala007@gmail.com>
2023-08-30 17:53:41 +0530

    my-app

---
+++ src/Functioncomp/Apipost.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+
+const Apipost = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState([]);
+
+  function savedata() {
+    // console.log(name, email, password);
+    let data = { name, email, password };
+
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((result) => {
+      console.log(result);
+      result.json().then((resp) => {
+        console.log(resp);
+      });
+    });
+  }
+  useEffect(() => {
+    fetch("http://localhost:5000/user").then((data) => {
+      data.json().then((resp) => {
+        console.log(resp);
+        setUser(resp);
+      });
+    });
+  },[]);
+
+  return (
+    <>
+      <h1>api post</h1>
+      <form>
+        <table width="50%" align="center">
+          <tr>
+            <td>
+              <label htmlFor="">Username</label>
+            </td>
+            <td>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="enter your name"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="">email</label>
+            </td>
+            <td>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="enter your email"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="">password</label>
+            </td>
+            <td>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="enter your password"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button onClick={savedata}>submit</button>
+            </td>
+          </tr>
+        </table>
+        <br />
+        <br />
+        <table border="1" width="70%" align="center">
+        <tr>
+          <td>name</td>
+          <td>email</td>
+          <td>password</td>
+          {/* <td>completed</td> */}
+        </tr>
+        {user.map((item) => (
+          <tr>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.password}</td>
+            {/* <td>{item.completed == true ? "True" : "False"}</td> */}
+          </tr>
+        ))}
+      </table>
+      </form>
+    </>
+  );
+};
+
+export default Apipost;

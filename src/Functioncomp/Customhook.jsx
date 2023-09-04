67c77b2acfacf674058ca42d9369bfa03e0337e2
ddarshit <darshitdayala007@gmail.com>
2023-09-04 11:56:29 +0530

    my-app

---
+++ src/Functioncomp/Customhook.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Custommycomp from './Custommycomp';
+import CustommycompTwo from './CustommycompTwo';
+
+const Customhook = () => {
+    return ( 
+        <>
+        <Custommycomp/>
+        <CustommycompTwo/>
+        </>
+     );
+}
+ 
+export default Customhook;

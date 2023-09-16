3559b0856530ee8211bdba3a12f0df3e8592e43c
ddarshit <darshitdayala007@gmail.com>
2023-09-16 18:45:23 +0530

    my-app

--- src/Functioncomp/Crud.jsx
+++ src/Functioncomp/Crud.jsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react'
 const Crud = () => {
     const [name,setName] = useState('')
     const [alldata , setAlldata] = useState([])
+    const [show ,setShow] = useState(false)
+    const [edittext ,setEdittext] = useState()
 
 
     const handleadd = () => {
@@ -12,21 +14,37 @@ const Crud = () => {
         if (name.length !== 0) {
             setAlldata(newdata => [...newdata, name])
             setName('')
-
         }
     }
-    console.log(alldata)
+    // console.log(alldata)
+    const handledelete = (index) => {
+        console.log(index)
+        alldata.splice(index,1)
+        setAlldata([...alldata])
+    }
+    const handleedit = (i) => {
+        setName(alldata[i])
+        setShow(true)
+        setEdittext(i)
+    }
+    const handleupdate = () =>{
+        alldata.splice(edittext,1,name)
+        setAlldata([...alldata])
+        setName('')
+        setShow(false)
+    }
     return ( 
         <>
         <div className='mx-5'>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            <button onClick={handleadd}>Add-Item</button>
+            {!show ? <button onClick={handleadd}>Add-Item</button> :
+            <button onClick={handleupdate}>update-Item</button>}
         </div>
 
         {
-            alldata.map((valu) =>
+            alldata.map((valu,i) =>
             
-            <div>
+            <div className='mx-5'>
                 <h1>{valu}</h1>
                 <button onClick={() => handleedit(i)}>edit</button>
                 <button onClick={() => handledelete(i)}>delete</button>
